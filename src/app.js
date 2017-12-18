639f2baaed9080adce343c25935a4c6a2ae51074
kevin <kevindenight@gmail.com>
2017-12-18 18:16:29 +0800

    config jshint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-import _ from "lodash"
+import _ from "lodash";
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
 
 console.log(_.join(['test', 'lodash']));
+
+if (x != true) {
+    console.log("xxx");
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,28 @@ const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
-    plugins : [
+    module: {
+        rules: [{
+            test: /\.js$/, // include .js files
+            enforce: "pre", // preload the jshint loader
+            exclude: /node_modules/, // exclude any and all files in the node_modules folder
+            use: [{
+                loader: "jshint-loader",
+                options: {
+                    camelcase: true,
+                    emitErrors: false,
+                    failOnHint: false,
+                    undef: true,
+                    unused: true,
+                    globals: [
+                        'console'
+                    ],
+                    esversion: '6'
+                }
+            }]
+        }]
+    },
+    plugins: [
         new webpack.HotModuleReplacementPlugin()
     ],
     devtool: 'inline-source-map',
