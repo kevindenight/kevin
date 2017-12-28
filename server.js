3be7ebe1196aa5fe2011a038cd7c76692d1a3af1
kevin <kevindenight@gmail.com>
2017-12-28 17:26:15 +0800

    fix server.js

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ const app = express();
 const config = require('./webpack.dev.js');
 const compiler = webpack(config);
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
-}));
+/*
+ * Tell express to use the webpack-dev-middleware and use the webpack.config.js
+ * configuration file as a base.
+ */
+app.use(webpackDevMiddleware(
+    compiler,
+    {'publicPath': config.output.publicPath}
+));
 
 app.use(webpackHotMiddleware(compiler));
 
 // Serve the files on port 3000.
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!\n');
-});
+app.listen(3000);
