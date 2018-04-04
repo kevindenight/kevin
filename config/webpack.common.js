357c18cec32005f34e3e5416b11898d3c0e0793e
kevin <kevindenight@gmail.com>
2018-04-04 11:42:52 +0800

    adjust webpack common conf

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -52,7 +52,7 @@ module.exports = {
         ]
     },
     'output': {
-        'path': path.resolve(__dirname, 'dist'),
+        'path': path.resolve(__dirname, '../dist'),
         'publicPath': '/'
     },
     'plugins': [
